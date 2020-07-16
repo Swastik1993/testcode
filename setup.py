eab857070de5e43198fd4ae484256b74000b5cad
Swastik Biswas <sbiswas1993@gmail.com>
2020-07-16 12:04:48 +0530

    python files added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='testcode',
+    version='0.0.1',
+    description='My private package from github repo',
+    url='git@github.com:Swastik1993/testcode.git',
+    author='Swastik Biswas',
+    author_email='sbiswas1993@gmail.com',
+    license='unlicense',
+    packages=['testcode'],
+    zip_safe=False
+)
